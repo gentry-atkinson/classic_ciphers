372e64a7584fd004ffa6f2c16ab55dc0c619b354
Gentry Atkinson <gma23@txstate.edu>
2019-05-17 17:14:37 -0500

    starting vigenere

---
+++ vigenere.cpp
@@ -0,0 +1,18 @@
+/*******************************
+*Author: Gentry Atkinson
+*Date: 17 May 2019
+*Desc: enciphers a textfile according to the Vigenere enciphers
+*******************************/
+
+#include <iostream>
+#include <fsteram>
+
+int main (int argc, char** argv){
+  switch(argc){
+    case 1:
+      cout << "" << endl;
+      break;
+    default:
+      break;
+  }
+}
