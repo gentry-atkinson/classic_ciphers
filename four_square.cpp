073fd9d73e3b10dc0a95f24a2b2fcfe0ba16dd01
Gentry Atkinson <gma23@txstate.edu>
2019-05-23 16:16:30 -0500

    starting new cipher

---
+++ four_square.cpp
@@ -0,0 +1,7 @@
+/*******************************
+*Author: Gentry Atkinson
+*Date: 23 May 2019
+*Desc: enciphers a textfile according to the Four Square cipher. May add a
+*  random char to the end of the file since this cipher requires an even number
+*  of characters in the file.
+*******************************/
