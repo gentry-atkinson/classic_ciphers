6fb03717b4d85a818927647f002ea9a77a735985
Gentry Atkinson <gma23@txstate.edu>
2020-07-13 13:49:58 -0500

    starting new cipher

---
+++ keyword_caesar.cpp
@@ -0,0 +1,9 @@
+/*******************************
+*Author: Gentry Atkinson
+*Date: 13 May 2020
+*Desc: this command line tool will enchipher or decipher a text document using
+*  a user-input keyword.
+*Example:
+*  Plain: THIS IS MY PLAINTEXT
+*  Key:   RATR AT RA TRATRATRA 
+*******************************/
